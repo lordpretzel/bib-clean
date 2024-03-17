c62f733f78bab6d4369a41743bffbedb29705ba2
Boris Glavic <lordpretzel@gmail.com>
2024-03-17 11:56:45 -0500

    bib clean

--- flake.nix
+++ flake.nix
@@ -63,6 +63,7 @@
                 '';                
               };
             };
+            
             ###################################################################
             #                       running                                   #
             ###################################################################
@@ -83,65 +84,12 @@
                 ];
                 runtimeInputs = [ mypython ];
                 shellHook = ''
-                  echo "Using virtual environment with Python ${mypython.python} with packages ${requirements-as-text}" | chara say
+                  echo "Using virtual environment with Python
+$(python --version)
+with packages
+${requirements-as-text}" | chara say
                 '';
               };
           }
       );
 }
-  
-#   outputs = { self, nixpkgs, flake-utils, mach-nix, ... }@inputs:
-#     flake-utils.lib.eachDefaultSystem
-#       (system:
-#         let
-#           pkgs = import nixpkgs {
-#             inherit system;
-#           };
-
-#           requirements-txt = "${self}/requirements.txt";
-
-#           # Utility to run a script easily in the flakes app
-#           simple_script = name: add_deps: text: let
-#             exec = pkgs.writeShellApplication {
-#               inherit name text;
-#               runtimeInputs = with pkgs; [
-#                 (mach-nix.lib."${system}".mkPython {
-#                   requirements = builtins.readFile requirements-txt;
-#                 })
-#               ] ++ add_deps;
-#             };
-#           in {
-#             type = "app";
-#             program = "${exec}/bin/${name}";
-#           };
-
-#           pyscript = "${self}/clean_bib.py";
-
-#         in with pkgs;
-#           {
-#             ###################################################################
-#             #                       running                                   #
-#             ###################################################################
-#             apps = {
-#               default = simple_script "pyscript" [] ''
-#                 python ${pyscript} "''$@"
-#               '';
-#             };
-
-#             ###################################################################
-#             #                       development shell                         #
-#             ###################################################################
-#             devShells.default = mach-nix.lib."${system}".mkPythonShell # mkShell
-#               {
-#                 # requirementx
-#                 requirements = builtins.readFile requirements-txt;
-#                 # python version
-#                 # nativeBuildInputs = with pkgs; [
-#                 #   python38
-#                 #   python38Packages.pip
-#                 #   poetry
-#                 # ];
-#               };
-#           }
-#       );
-# }
